7aa4511a86f42961ee2b6edafed808ea08a40a8b
Deba <aldealdo@gmail.com>
2017-11-03 14:36:22 +0100

    Ajout des fichiers

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Le Tamaris ASBL</title>
+
+		<!-- Bootstrap CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Concert+One|Cormorant+Garamond:400,700,700i|Gochi+Hand|Work+Sans:400,500,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+
+		<div class="navbar navbar-default" role="navigation">
+
+			<div class="container">
+				<div class="logo">
+					<img src="images/logo.translucide.cropped.png" alt="Le Tamaris ASBL">
+				</div>
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="#">ACCUEIL</a>
+						</li>
+						<li>
+							<a href="#">A&nbsp;PROPOS</a>
+						</li>
+						<li>
+							<a href="#">MAISONS</a>
+						</li>
+						<li>
+							<a href="#">CONTACTS</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="container box">
+			<!-- ********* INTRODUCTION ********* -->
+			<div class="row">
+				<div class="inner-wrapper">
+					<div class="col-md-6 col-lg-8 hidden-xs">
+						<div class="big-card">
+							<div class="sub-card"></div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-4">
+						<div class="card bg-beige">
+							<span class="icone">
+								<img src="icons/hand-shake.svg">
+							</span>
+							<span class="title">
+								Ipsum
+							</span>
+							<div class="text">
+								Excepteur sint occaecat cupidatat non proident.
+
+							</div>
+						</div>
+						<div class="card bg-jaune">
+							<span class="icone">
+								<img src="icons/social-services.svg">
+							</span>
+							<span class="title">
+								Lorem
+							</span>
+							<div class="text">
+								Duis aute irure dolor in reprehenderit.
+							</div>
+						</div>
+						<div class="card bg-vert">
+							<span class="icone">
+								<img src="icons/panel.svg">
+							</span>
+							<span class="title">
+								Fiatae
+							</span>
+							<div class="text">
+								Excepteur sint occaecat cupidatat non proident.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>		<!-- ROW -->
+			<!-- ********* CITATION ********* -->
+			<div class="row bg-orange">
+				<div class="inner-wrapper">
+					<div class="citation">
+						<span>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</span>
+					</div>
+				</div>
+			</div><!-- ROW -->
+			<!-- ********* TYPOGRAPHIE ********* -->
+			<div class="row">
+				<div class="inner-wrapper">
+					<div class="content">
+						<div class="heading">
+							<h1 class="bg-vert center-block">HEADING</h1>
+						</div>
+						<h2>SubHeading</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Aliquam nulla ipsum, commodo vitae tellus et, commodo pulvinar leo. Etiam elementum erat malesuada, pellentesque justo non, pellentesque nunc. Pellentesque a egestas risus. Vivamus eu arcu metus. In a dui suscipit, ullamcorper nulla nec, consequat ex. Aenean porta tortor eget massa auctor consequat. Quisque sit amet placerat tortor. Etiam luctus ipsum sed sapien fringilla fermentum. Nulla facilisi. Nullam gravida justo id magna molestie, ultricies tempor est tincidunt.
+
+Ut ultricies dui sit amet tellus egestas finibus. Pellentesque varius, mi ac pellentesque ullamcorper, arcu purus auctor velit, id molestie urna mi id purus. Maecenas non tortor vitae tellus suscipit aliquam vitae eget magna. Integer in pretium lorem. Aliquam sed neque molestie, lobortis enim id, cursus felis. Vestibulum congue gravida nulla. Vestibulum tristique mauris eu ante pulvinar iaculis. Nunc pharetra ante eu eros dignissim convallis.</p>
+					</div>
+				</div>
+			</div><!-- ROW -->
+			<!-- ********* TYPOGRAPHIE 2 ********* -->
+			<div class="row">
+				<div class="inner-wrapper">
+					<div class="col-sm-4">
+						<h3>SubHeading</h3>
+						<p>Quisque sit amet placerat tortor. Etiam luctus ipsum sed sapien fringilla fermentum. Nulla facilisi. Nullam gravida justo id magna molestie, ultricies tempor est tincidunt. Vivamus eu arcu metus. In a dui suscipit, ullamcorper nulla nec, consequat ex. Aenean porta tortor eget massa auctor consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Aliquam nulla ipsum, commodo vitae tellus et, commodo pulvinar leo. Etiam elementum erat malesuada, pellentesque justo non, pellentesque nunc. Pellentesque a egestas risus. Vivamus eu arcu metus. In a dui suscipit, ullamcorper nulla nec, consequat ex. Aenean porta tortor eget massa auctor consequat. Quisque sit amet placerat tortor. Etiam luctus ipsum sed sapien fringilla fermentum. Nulla facilisi. Nullam gravida justo id magna molestie, ultricies tempor est tincidunt.</p>
+					</div>
+					<div class="col-sm-8">
+						<h3>SubHeading</h3>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Aliquam nulla ipsum, commodo vitae tellus et, commodo pulvinar leo. Etiam elementum erat malesuada, pellentesque justo non, pellentesque nunc. Pellentesque a egestas risus. Vivamus eu arcu metus. In a dui suscipit, ullamcorper nulla nec, consequat ex. Aenean porta tortor eget massa auctor consequat. Quisque sit amet placerat tortor. Etiam luctus ipsum sed sapien fringilla fermentum. Nulla facilisi. Nullam gravida justo id magna molestie, ultricies tempor est tincidunt.</p>
+						<h4>Choses à faire:</h4>
+						<ul>
+							<li class="green-dot">Donner de l'eau aux plantes</li>
+							<li class="green-dot">Ranger et nettoyer la maison</li>
+							<li class="green-dot">Faire ceci cela et son contraire</li>
+						</ul>
+					</div>
+				</div>
+			</div><!-- ROW -->
+			<!-- WHITE SPACE -->
+			<div class="row white-space"></div>
+			<!-- ********* CITATION 2 ********* -->
+			<div class="row bg-vert">
+				<div class="inner-wrapper">
+					<div class="citation">
+						<span>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</span>
+					</div>
+				</div>
+			</div><!-- ROW -->
+			<!-- ********* CAROUSEL ********* -->
+			<div class="row">
+				<div id="full-width" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#full-width" data-slide-to="0" class="active"></li>
+						<li data-target="#full-width" data-slide-to="1"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="item active">
+							<img data-src="images/education.jpg" src="images/education.jpg">
+						</div>
+						<div class="item">
+							<img data-src="images/learning.png" alt="Second slide" src="images/learning.png">
+						</div>
+					</div>
+					<a class="left carousel-control" href="#full-width" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+					<a class="right carousel-control" href="#full-width" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>
+			</div><!-- ROW -->
+			<!-- WHITE SPACE -->
+			<div class="row white-space"></div>
+			<!-- ********* BLOG BLOG ********* -->
+			<div class="row">
+				<div class="inner-wrapper">
+					<div class="col-sm-12">
+						<h3>Dernières Nouvelles</h3>
+						<div class="col-sm-6 col-md-4">
+							<div class="blogitem">
+								<div class="thumb">
+									<img src="images/acro.jpg" alt="">
+								</div>
+								<h4>Accrobranche à Durbuis</h4>
+								<span class="author">Jacqueline Machin</span>
+								<span class="separator">|</span>
+								<span class="date">Ven. 27 Juin 2017</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Clickatum ici.</p>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-4">
+							<div class="blogitem">
+								<div class="thumb">
+									<img src="images/montagne.jpeg" alt="">
+								</div>
+								<h4>Sortie dans les Pyrénées</h4>
+								<span class="author">Charles Dupont</span>
+								<span class="separator">|</span>
+								<span class="date">Ven. 27 Juin 2017</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Clickatum ici.</p>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-4">
+							<div class="blogitem">
+								<div class="thumb">
+									<img src="images/acro.jpg" alt="">
+								</div>
+								<h4>Sortie dans les Pyrénées</h4>
+								<span class="author">Jacqueline Machin</span>
+								<span class="separator">|</span>
+								<span class="date">Ven. 27 Juin 2017</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec risus at sapien tincidunt porttitor. Curabitur orci magna, pulvinar eu risus a, molestie imperdiet nisl. Clickatum ici.</p>
+							</div>
+						</div>
+					</div><!-- col-sm-12 -->
+				</div>
+			</div><!-- ROW -->
+			<!-- WHITE SPACE -->
+			<div class="row white-space"></div>
+			<!-- ********* FORMULAIRE DE CONTACT ********* -->
+			<div class="row bg-oddblue">
+				<div class="col-sm-12">
+					<div class="heading">
+						<h1 class="bg-vert center-block">CONTACT</h1>
+						<div class="col-sm-4 col-sm-offset-2">
+							<address>
+							  	Le Tamaris ASBL<br>
+								41, Avenue Edouard de Thibault<br>
+								1040 - Bruxelles
+							</address>
+						</div>
+						<div class="col-sm-4 text-right">
+							<address>
+								<abbr title="Phone">Téléphone:</abbr> +32 2 734 90 33<br>
+								<abbr title="Phone">Fax(s):</abbr> +32 2 640 20 73<br>
+								+32 2 742 24 73
+							</address>
+						</div>
+						<div class="col-sm-8 col-sm-offset-2">
+							<form action="" method="POST" role="form" class="myForm">
+
+								<div class="form-group">
+									<input type="text" name="prenom" id="prenom" class="form-control input-lg" required="required" placeholder="Prénom">
+								</div>
+								<div class="form-group">
+									<input type="text" name="email" id="email" class="form-control input-lg" required="required" placeholder="E-mail">
+								</div>
+								<div class="form-group">
+									<textarea name="message" id="message" class="form-control input-lg" rows="3" required="required" placeholder="Laissez ici votre message"></textarea>
+								</div>
+								<div class="form-group text-right">
+									<button type="submit" class="btn btn-primary btn-lg">Envoyer</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div><!-- ROW -->
+			<!-- ********* PRE FOOTER ********* -->
+			<div class="row bg-vert">
+				<div class="inner-wrapper">
+					<p>
+						Last considerations. 
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+					</p>
+				</div>
+			</div><!-- ROW -->
+			<!-- ********* FOOTER TEXT ********* -->
+			<div class="row footer">
+				<p>LE TAMARIS ASBL © 2017</p>
+			</div>
+			
+
+		</div><!-- CONTAINER BOX -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</body>
+</html>
